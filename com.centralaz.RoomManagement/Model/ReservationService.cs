2dcd1d8bdf258531e8da4755f09a8898c04ba406
nairdo <nairdo@gmail.com>
2019-11-23 18:46:49 -0700

    + [RM] Added a relationship to EventItem on the Reservation model and added support for filtering reservations in the ReservationList block by configuring a query string block setting.

--- com.centralaz.RoomManagement/Model/ReservationService.cs
+++ com.centralaz.RoomManagement/Model/ReservationService.cs
@@ -877,6 +877,7 @@ namespace com.centralaz.RoomManagement.Model
             target.ScheduleId = source.ScheduleId;
 
             target.CampusId = source.CampusId;
+            target.EventItemId = source.EventItemId;
             target.ReservationMinistryId = source.ReservationMinistryId;
 
             //target.ApprovalState = source.ApprovalState;
